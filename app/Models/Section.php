96b96e93a42bd3704aa69ce0303d6485dc10c977
Polash Mahmud <polashmahmud@gmail.com>
2024-12-12 15:08:36 +0600

    fix seeder file

---
+++ app/Models/Section.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Section extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['class_id', 'name'];
+
+    public function class(): BelongsTo
+    {
+        return $this->belongsTo(Classes::class);
+    }
+
+    public function students(): HasMany
+    {
+        return $this->hasMany(Student::class);
+    }
+}
+
+
