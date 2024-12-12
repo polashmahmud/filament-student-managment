d2e189926d3c721270858ef3b38c92163907b0f0
Polash Mahmud <polashmahmud@gmail.com>
2024-12-13 00:31:59 +0600

    feat: Add LatestStudents and StatsOverview widgets
    Introduced two new widgets in the Filament dashboard:
    - LatestStudents displays the latest students with their names, emails, classes, and sections.
    - StatsOverview provides an overview of total classes, sections, and students.

---
+++ app/Filament/Widgets/LatestStudents.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Student;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class LatestStudents extends BaseWidget
+{
+    protected static ?int $sort = 2;
+
+    protected int | string | array $columnSpan = 'full';
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                Student::query()
+                    ->latest()
+                    ->with('class', 'section')
+            )
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('class.name')
+                    ->searchable()
+                    ->badge(),
+                Tables\Columns\TextColumn::make('section.name')
+                    ->searchable()
+                    ->badge(),
+            ]);
+    }
+}
